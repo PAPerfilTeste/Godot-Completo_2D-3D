112c5740f4d31ccb283395f6b7c0f21eec5f3ce0
P.Arthur <110242063+PArthur006@users.noreply.github.com>
2024-11-12 23:56:10 -0300

    Initial commit

---
+++ Projeto Teste/node_2d.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+var nome = "Pedro Arthur"
+var pontos = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Olá, ", nome, "! Bem vindo ao jogo.")
+	print("Seus pontos: ", pontos)
+	pass # Replace with function body.
---
+++ README.md
@@ -0,0 +1,2 @@
+# Games-Godot_Completo
+ Material e projetos desenvolvidos durante o curso de desenvolvimento de jogos 2D e 3D com a Godot Engine. Inclui scripts, mecânicas e design de jogos para criação de projetos completos.
